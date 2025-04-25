9aafea2ab49b69e28e90b72b7b9d4b1b5372737e
raul <lic.raultrujillo@gmail.com>
2025-04-25 17:48:04 -0600

    se agrega #

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -82,7 +82,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
   providers: [],
 })
